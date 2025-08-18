6d0b096fc791bcb17c54c8c903afa0cd39da14fa
Pulfa <markobelaich2004@gmail.com>
2025-08-18 20:46:55 -0300

    avance incompleto
    hasta min 31

--- TowerFunOnline(TFO)/Assets/Scrips/GameStarter.cs
+++ TowerFunOnline(TFO)/Assets/Scrips/GameStarter.cs
@@ -2,10 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
+ 
 
 public class GameStarter : MonoBehaviourPunCallbacks
 {
-    
+    [SerializeField] private PhotonView playerPrefab;
+    [SerializeField] private Transform playerSpawn;
+    [SerializeField] private List<Transform> playerSpawnPsoition = new List<Transform>();
+
+    private int currentSpawnIndex = 0;
+
+
     void Start()
     {
         PhotonNetwork.JoinRandomRoom();
@@ -14,8 +22,29 @@ public class GameStarter : MonoBehaviourPunCallbacks
     public override void OnJoinedRoom()
     {
 
-     //   GameObject player = PhotonNetwork.Instantiate(playerPrefab.name,);
+      GameObject player = PhotonNetwork.Instantiate(playerPrefab.name,GetPlayerSpawnPosition().position,playerSpawn.rotation);
         
     }
+
+    private IEnumerator WaitForSpawnPoint() 
+    {
+
+        if (!PhotonNetwork.IsMasterClient)
+        {
+            yield return new WaitUntil(() => currentSpawnIndex > -1);
+
+        }
+        else
+        {
+            currentSpawnIndex = 0;
+        }
+
+
+        CreateAndSetupPlayerInstance();
+
+        UpdateAndSetupPlayerInstance();
+    
+    }
+
 
 }
--- TowerFunOnline(TFO)/Assets/Scrips/MenuPun.cs
+++ TowerFunOnline(TFO)/Assets/Scrips/MenuPun.cs
@@ -2,27 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.UI;
+using TMPro;
 using UnityEngine.SceneManagement;
-using Photon.Pun.Demo.Cockpit;
+
 
 public class MenuPun : MonoBehaviourPunCallbacks
 {
+    public string gameSceneName;
+    public TMP_InputField inputField;
+    public Button connectionButton;
 
     private const string nicknameKey = "playerNickname";
-    private string nickname = "Prueba";
-    public void ConectButton()
+    private string nickname;
+
+
+
+    private void Start()
+    {
+        connectionButton.onClick.AddListener(HandleConnectButton);
+        inputField.onValueChanged.AddListener(VerifyName);
+    }
+
+    private void VerifyName(string newName)
+    {
+        if (inputField.text.Length == 0)
+        {
+            connectionButton.interactable = false;
+        }
+
+        if (inputField.text.Length >= 1 && !connectionButton.interactable)
+        {
+            connectionButton.interactable = true;
+        }
+        nickname = newName;
+    }
+
+    public void HandleConnectButton()
     {
         PlayerPrefs.SetString(nicknameKey, nickname);
         PhotonNetwork.NickName = nickname.ToUpper();
+        print(nickname+ "intenta conectarse");
         PhotonNetwork.ConnectUsingSettings();
-
+        connectionButton.interactable = false;
 
     }
 
+   
     public override void OnConnectedToMaster()
     {
         Debug.Log("conectando al master.....");
-        base.OnConnectedToMaster();
-        SceneManager.LoadScene(1);
+        
+        SceneManager.LoadScene(gameSceneName);
     }
 }
