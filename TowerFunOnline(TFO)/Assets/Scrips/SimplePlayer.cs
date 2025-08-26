e5b1f1ed4d9a5d3621630be3d75af1f433db1771
pipesave <131900965+pipesave@users.noreply.github.com>
2025-08-25 22:11:53 -0300

    se vuelve para atraas

--- TowerFunOnline(TFO)/Assets/Scrips/SimplePlayer.cs
+++ TowerFunOnline(TFO)/Assets/Scrips/SimplePlayer.cs
@@ -22,7 +22,7 @@ public class SimplePlayer : MonoBehaviourPunCallbacks
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void FixedUpdate()
+    void Update()
     {
         if (!photonView.IsMine) return;
 
