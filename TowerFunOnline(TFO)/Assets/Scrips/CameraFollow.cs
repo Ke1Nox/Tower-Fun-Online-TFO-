eeefa0594b9583a65666d54c9407d7013583142a
pipesave <131900965+pipesave@users.noreply.github.com>
2025-09-07 18:18:18 -0300

    camara integrada en players
    camara para cada player (prueba)

---
+++ TowerFunOnline(TFO)/Assets/Scrips/CameraFollow.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using Photon.Pun;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Vector3 offset = new Vector3(0, 2, -10);
+    private Transform target;
+
+    void Start()
+    {
+        // Buscar al jugador local cuando aparezca
+        FindLocalPlayer();
+    }
+
+    void LateUpdate()
+    {
+        if (target == null)
+        {
+            FindLocalPlayer();
+            return;
+        }
+
+        // seguir suavemente al target
+        Vector3 desiredPosition = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * 5f);
+    }
+
+    void FindLocalPlayer()
+    {
+        // Busca todos los jugadores en la escena
+        SimplePlayer[] players = FindObjectsOfType<SimplePlayer>();
+        foreach (var p in players)
+        {
+            PhotonView view = p.GetComponent<PhotonView>();
+            if (view != null && view.IsMine)
+            {
+                target = p.transform;
+                break;
+            }
+        }
+    }
+}
