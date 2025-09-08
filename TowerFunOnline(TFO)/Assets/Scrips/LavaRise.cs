35b739657eb8f2371b80fcef5e14b07a6c24b24d
pipesave <131900965+pipesave@users.noreply.github.com>
2025-09-08 15:20:34 -0300

    refix lava

--- TowerFunOnline(TFO)/Assets/Scrips/LavaRise.cs
+++ TowerFunOnline(TFO)/Assets/Scrips/LavaRise.cs
@@ -6,14 +6,7 @@ public class LavaRise : MonoBehaviour
     [SerializeField] private float riseSpeed = 1f; // Velocidad constante hacia arriba
 
 
-    void Start()
-    {
-        if (PhotonNetwork.IsMasterClient)
-        {
-            // Arranca desactivada, la activa el GameReadyManager
-            enabled = false;
-        }
-    }
+  
 
     void Update()
     {
